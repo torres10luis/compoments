5fa80a00e0f0ef862fd3a5c67cf956b3d19e1460
torres10luis <Luis.angel_15@yahoo.com>
2018-05-31 10:15:37 -0700

    day2practice

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
 
   constructor() { }
-
+    setTimeout(() => {
+  );
   ngOnInit() {
   }
 
